6b33eeffd97d756135087536acad7c523196e586
Hiruna Jayamanne <hiru@hiru.dev>
2020-09-15 00:13:29 +1200

    Add markdown support for the webclient

--- frontend/src/Message.tsx
+++ frontend/src/Message.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import ReactMarkdown from "react-markdown"
 
 type MessageProps = {
 	user: string,
@@ -10,7 +11,16 @@ function Message(props: MessageProps) {
 	return (
 		<div className="message">
 			<div className="message-user">{props.user}:</div>
-			<div className="message-text">{props.text}</div>
+			<ReactMarkdown
+				className="message-contents"
+				source={props.text}
+				escapeHtml={true}
+				allowedTypes={[
+					"root", "text", "break", "paragraph", "emphasis", "strong", "thematicBreak", "blockquote", "delete",
+					"link", "image", "table", "tableHead", "tableBody", "tableRow", "tableCell", "list", "listItem",
+					"inlineCode", "code"
+				]}
+			/>
 			<div className="message-date">{props.date}</div>
 		</div>
 	)
